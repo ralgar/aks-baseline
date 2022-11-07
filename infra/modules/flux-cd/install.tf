acab640805d5559edc74ec225f37f28ffb9e86e8
Ryan Algar <59636191+ralgar@users.noreply.github.com>
2022-11-07 12:25:45 -0800

    Fix: Terraform flux-cd module resource ordering
    Terraform was occasionally failing to apply the manifests due to the
    namespace resource not being created first.

--- infra/modules/flux-cd/install.tf
+++ infra/modules/flux-cd/install.tf
@@ -4,8 +4,26 @@ data "kubectl_file_documents" "flux_install" {
   content = file("${path.root}/../cluster/system/flux-cd/flux-install.yaml")
 }
 
+# Apply the namespace first, solving resource ordering issue
+resource "kubectl_manifest" "flux_namespace" {
+  yaml_body = <<YAML
+apiVersion: v1
+kind: Namespace
+metadata:
+  labels:
+    app.kubernetes.io/instance: flux-system
+    app.kubernetes.io/part-of: flux
+    app.kubernetes.io/version: v0.36.0
+    pod-security.kubernetes.io/warn: restricted
+    pod-security.kubernetes.io/warn-version: latest
+  name: flux-system
+YAML
+  apply_only = true
+}
+
 # Apply manifests
 resource "kubectl_manifest" "flux_install" {
-  for_each  = data.kubectl_file_documents.flux_install.manifests
-  yaml_body = each.value
+  for_each   = data.kubectl_file_documents.flux_install.manifests
+  yaml_body  = each.value
+  depends_on = [ kubectl_manifest.flux_namespace ]
 }

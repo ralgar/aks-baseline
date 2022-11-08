5f22d152041d901c525a7b3b9c288f43d888896a
Ryan Algar <59636191+ralgar@users.noreply.github.com>
2022-11-07 18:24:40 -0800

    Finalize removal of unused ArgoCD deployment

--- infra/main.tf
+++ infra/main.tf
@@ -9,12 +9,3 @@ module "aks_cluster" {
 module "flux_cd" {
   source = "./modules/flux-cd"
 }
-
-/*resource "helm_release" "argocd" {
-  name              = "argocd"
-  namespace         = "argocd"
-  chart             = "${path.root}/../cluster/system/argocd"
-  dependency_update = true
-  create_namespace  = true
-  atomic            = true
-}*/

8f49e34a427a7260c9d9e1bce1d4cec609fb4c07
Ryan Algar <59636191+ralgar@users.noreply.github.com>
2022-11-07 20:28:31 -0800

    Improve Terraform root module input variables

--- infra/main.tf
+++ infra/main.tf
@@ -1,9 +1,9 @@
 module "aks_cluster" {
   source = "./modules/azure/aks-cluster"
 
-  environment = "staging"
-  prefix      = "techdemo"
+  environment = var.environment
   location    = var.location
+  prefix      = var.prefix
 }
 
 module "flux_cd" {
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,4 +1,17 @@
+variable "environment" {
+  description = "Deployment environment (prod, staging, demo)."
+  default     = "demo"
+  type        = string
+}
+
 variable "location" {
   description = "Location (region) for resources."
   default     = "West US 3"
+  type        = string
+}
+
+variable "prefix" {
+  description = "Prefix for naming resources."
+  default     = "techdemo"
+  type        = string
 }

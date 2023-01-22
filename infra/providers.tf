a3148b3c7b3aca4fc28e2f57ec18c7b6351fccf6
Ryan Algar <59636191+ralgar@users.noreply.github.com>
2023-01-22 14:25:10 -0800

    Fix: providers.tf formatting

--- infra/providers.tf
+++ infra/providers.tf
@@ -2,12 +2,11 @@ terraform {
   required_version = ">=1.0"
 
   required_providers {
-    azuread    = { source = "hashicorp/azuread" }
-    azurerm    = { source = "hashicorp/azurerm" }
-    flux       = { source = "fluxcd/flux" }
-    helm       = { source = "hashicorp/helm" }
-    kubectl    = { source = "gavinbunney/kubectl" }
-    kubernetes = { source = "hashicorp/kubernetes" }
+    azuread = { source = "hashicorp/azuread" }
+    azurerm = { source = "hashicorp/azurerm" }
+    flux    = { source = "fluxcd/flux" }
+    helm    = { source = "hashicorp/helm" }
+    kubectl = { source = "gavinbunney/kubectl" }
   }
 }
 
@@ -22,24 +21,17 @@ provider "azurerm" {
 
 provider "helm" {
   kubernetes {
-    host = module.aks_cluster.host
-    client_certificate = base64decode(module.aks_cluster.client_certificate)
-    client_key = base64decode(module.aks_cluster.client_key)
+    host                   = module.aks_cluster.host
+    client_certificate     = base64decode(module.aks_cluster.client_certificate)
+    client_key             = base64decode(module.aks_cluster.client_key)
     cluster_ca_certificate = base64decode(module.aks_cluster.cluster_ca_certificate)
   }
 }
 
 provider "kubectl" {
-  host = module.aks_cluster.host
-  client_certificate = base64decode(module.aks_cluster.client_certificate)
-  client_key = base64decode(module.aks_cluster.client_key)
+  host                   = module.aks_cluster.host
+  client_certificate     = base64decode(module.aks_cluster.client_certificate)
+  client_key             = base64decode(module.aks_cluster.client_key)
   cluster_ca_certificate = base64decode(module.aks_cluster.cluster_ca_certificate)
-  load_config_file = false
-}
-
-provider "kubernetes" {
-  host = module.aks_cluster.host
-  client_certificate = base64decode(module.aks_cluster.client_certificate)
-  client_key = base64decode(module.aks_cluster.client_key)
-  cluster_ca_certificate = base64decode(module.aks_cluster.cluster_ca_certificate)
+  load_config_file       = false
 }
